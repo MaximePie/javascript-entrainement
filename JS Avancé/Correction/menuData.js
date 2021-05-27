ef8ee878bead8b8898b1a09e6d6274fd6da7f26a
Chatmalow <maxime.pie.mail@gmail.com>
2021-05-27 15:40:19 +0200

    feat: Correction des menus

---
+++ JS Avancé/Correction/menuData.js
@@ -0,0 +1,27 @@
+/*
+* Pour l'exercice 2, menu
+* */
+
+export const plats = new Map([
+  [
+    "France",
+    {
+      plat: "Baguette",
+      avecViande: false,
+    },
+  ],
+  [
+    "Espagne",
+    {
+      plat: "Paella",
+      avecViande: true,
+    },
+  ],
+  [
+    "Suède",
+    {
+      plat: "smörgås",
+      avecViande: true,
+    },
+  ],
+]);
