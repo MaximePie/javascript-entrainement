460505c734343b702b08fccc36c2f3beed3064c7
Chatmalow <maxime.pie.mail@gmail.com>
2021-10-27 13:13:31 +0200

    feat: add correction for 1-3

--- 1 - Premiers pas/niveau3Correction/script.js
+++ 1 - Premiers pas/niveau3Correction/script.js
@@ -1,4 +1,13 @@
 // Nous utilisons 'titre' car dans index.html, notre h1 a pour id 'titre'.
 const monTitre = document.getElementById('titre');
 
-monTitre.style.color = 'red';
+monTitre.style.color = 'blue';
+monTitre.style.background = 'green';
+// font-size (css) => fontSize (js)
+
+
+const monBouton = document.getElementById("changeMeButton");
+console.log(monBouton);
+monBouton.innerHTML = "Ce qu'on veut";
+// monBouton.innerHTML = "<a href='https://www.w3schools.com/js/tryit.asp?filename=tryjs_change_style'>Haha</a>";
+
